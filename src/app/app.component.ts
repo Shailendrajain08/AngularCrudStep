5eb5d975b31d91c1dd3fcc5ca730e109970f1e45
shailendra08 <shailendra.jain0894@gmail.com>
2024-01-02 16:13:50 -0500

    Angular Crud starting points and designing

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularCrud';
+
+  constructor(private _dialog: MatDialog) {
+
+
+  }
+
+  openAddEditForm(){
+    this._dialog.open(EmpAddEditComponent)
+
+  }
+}
